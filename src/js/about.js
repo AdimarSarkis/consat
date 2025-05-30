bff3c5021573bc5e754ace3180a73d9459b2da00
Adimar <sarkis170@gmail.com>
2025-05-30 14:13:04 -0300

    refactor: responsividade

--- src/js/about.js
+++ src/js/about.js
@@ -92,9 +92,7 @@ document.addEventListener("DOMContentLoaded", function () {
     milestoneElement.addEventListener("click", () => {
       if (window.innerWidth <= 768) {
         modalContent.innerHTML = `
-          <div class="icon"><svg width="39" height="39" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41Z"></path>
-</svg></div>
+          <div class="icon"></div>
           <h3 class="title">${milestone.title}</h3>
           <p class="details">${milestone.details}</p>
         `;
